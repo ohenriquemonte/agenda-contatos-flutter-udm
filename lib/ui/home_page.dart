b97066c6675479c2b495ad4b68499ffba9294ce7
Henrique Montenegro <henriquemontenegro@MacBook-Pro-de-Henrique.local>
2021-03-16 16:23:09 -0300

    100. Criando um Botão Flutuante

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -9,25 +9,45 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   ContactHelper helper = ContactHelper();
 
+  List<Contact> contacts = List();
+
   @override
   void initState() {
     super.initState();
 
-    Contact c = Contact();
-    c.name = "Henrique Montenegro";
-    c.email = "montenegrohenrique@gmail.com";
-    c.phone = "51999999999";
-    c.img = "imgTeste";
-
+    // Contact c = Contact();
+    // c.name = "Henrique Montenegro";
+    // c.email = "montenegrohenrique@gmail.com";
+    // c.phone = "51999999999";
+    // c.img = "imgTeste";
     // helper.saveContact(c);
 
-    helper.getAllContact().then((list) => {
-      print(list)
+    helper.getAllContact().then((list) {
+      setState(() {
+        contacts = list;
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contatos"),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.all(10),
+        itemBuilder: (context, index) {},
+        itemCount: contacts.length,
+      ),
+    );
   }
 }
